a9029ca14922e90d20fdeaf1a1fa1f383ce54b2b
Hashir Hussain <hashir.hussain@yash.com>
2021-05-15 00:37:21 +0530

    First code base

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-function spit(a) {
-  console.log(a);
+function getTime() {
+  const currentDate = new Date();
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  const ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12 ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  const strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
+function getType(input) {
+  if (Array.isArray(input)) {
+    return "array";
+  } else {
+    return typeof input;
+  }
+}
+
+function getValue(input) {
+  return JSON.stringify(input);
+}
+
+function spit(input, comment) {
+  const time = `Time: ${getTime()}`;
+  const type = `Type: ${getType(input)}`;
+  const value = `Value: ${getValue(input)}`;
+  const _comment = comment ? `Comment: ${comment}` : "Comment: --";
+  console.log(`${time}\n${type}\n${value}\n${_comment}`);
 }
 
 module.exports.spit = spit;
